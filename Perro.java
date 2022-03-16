c06f3c2ff1f5caed0823a7409b6fc964a00a46c6
ENGB02 <jarroyo@cifpfbmoll.eu>
2022-03-16 19:53:36 +0100

    rama1

--- Perro.java
+++ Perro.java
@@ -1,27 +1,26 @@
-package ED;
+Package ED;
 
 public class Perro extends Mascota {
     //Atributos
-    private String raza =new String();
+    private String raza = new String();
     //Constructores
-    public Perro(String raza){
-        this.raza=raza;
+    public Perro(String raza) {
+        this.raza = raza;
     }
-    public Perro(){
-        this.raza=null;
+    public Perro() {
+            this.raza = null;
+        }
+        //Gets y Sets
+    private void setRaza(String raza) {
+        this.raza = raza;
     }
-    //Gets y Sets
-    private void setRaza(String raza){
-        this.raza=raza;
-    }
-    private String getRaza(){
+    private String getRaza() {
         return this.raza;
     }
 
-    
+
     @Override
-    public void sonido(){
+    public void sonido() {
         System.out.println("La mascota hace sonidos ");
     }
-
-}
+    }
